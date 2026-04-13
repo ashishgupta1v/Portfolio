2c70d7c08b1b53bc10303da2101beb463f510e63
Ashish Gupta <ashishgupta1v@gmail.com>
2026-04-13 12:48:08 +0530

    Revert "perf: pre-render portfolio to static index and serve root from CDN"
    This reverts commit 320a9dbe23ce99473ac6e028dcc28593ee033d1c.

--- routes/console.php
+++ routes/console.php
@@ -1,39 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Contracts\Http\Kernel as HttpKernel;
-use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Artisan::command('portfolio:prerender', function () {
-    $outputPath = public_path('index.html');
-
-    /** @var HttpKernel $kernel */
-    $kernel = app(HttpKernel::class);
-    $request = Request::create('/', 'GET', [], [], [], [
-        'HTTP_ACCEPT' => 'text/html',
-        'HTTP_USER_AGENT' => 'ssg-prerender',
-    ]);
-
-    /** @var HttpResponse $response */
-    $response = $kernel->handle($request);
-
-    try {
-        if ($response->getStatusCode() !== 200) {
-            $this->error("Prerender failed with status {$response->getStatusCode()}.");
-            return self::FAILURE;
-        }
-
-        file_put_contents($outputPath, $response->getContent());
-        $this->info("Static page generated at {$outputPath}");
-
-        return self::SUCCESS;
-    } finally {
-        $kernel->terminate($request, $response);
-    }
-})->purpose('Pre-render the portfolio page into public/index.html for static hosting');
